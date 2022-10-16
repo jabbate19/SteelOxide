2b38e55a7dc66518250b5aa0ad779f7dbb8591fe
Joe Abbate <josephabbateny@gmail.com>
2022-10-16 16:58:32 -0400

    Windows Tracker Main

--- src/windows/tracker.rs
+++ src/windows/tracker.rs
@@ -1,4 +1,6 @@
-use crate::utils::PIDInfo;
+use crate::utils::{PIDInfo, yes_no, exec_cmd};
+use std::collections::HashSet;
+use std::fmt::Display;
 
 #[derive(Eq, Hash, PartialEq)]
 struct Socket {
@@ -31,7 +33,45 @@ impl Socket {
     }
 }
 
-pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("This is not yet implemented!");
-    Ok(())
+impl Display for Socket {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{} | {} | {} | {} | {}",
+            self.protocol,
+            self.local_addr,
+            self.foreign_addr,
+            self.state,
+            self.pid,
+        )
+    }
 }
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut safe: HashSet<Socket> = HashSet::new();
+    loop {
+        let netstat = exec_cmd("netstat", &["-noq"], false)
+            .unwrap()
+            .wait_with_output()
+            .unwrap();
+        if netstat.status.success() {
+            for line in String::from_utf8_lossy(&netstat.stdout).split("\n") {
+                let sock = match Socket::new(line.to_string()) {
+                    Ok(sock) => sock,
+                    Err(_) => continue,
+                };
+                let pid = sock.analyze_pid();
+                if !safe.contains(&sock) {
+                    println!("{}", sock);
+                    println!("{}", pid);
+                    if yes_no("Keep socket".to_string()) {
+                        safe.insert(sock);
+                    } else {
+                        pid.terminate();
+                    }
+                }
+            }
+        }
+    }
+}
+
